146091d005e01607a2a73ae36493e3847f359977
Kylie Sy <kylksy@gmail.com>
2015-11-02 01:00:41 +1100

    added route to create new stretch

--- main.rb
+++ main.rb
@@ -17,6 +17,16 @@ end
 
 # get form for new stretch
 get '/stretches' do
+	
+	erb :'/stretches/new'
+end
 
+
+# create new stretch
+post '/stretches' do
+	Stretch.create(
+		task: "#{ params[:stretch] }",
+		stretch_type_id: "#{ params[:stretch_type_id] }"
+	)
 	erb :'/stretches/new'
 end
