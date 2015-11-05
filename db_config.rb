ac734908beeac39657e920c7cf5313716fcff373
Kylie Sy <kylksy@gmail.com>
2015-11-05 17:18:21 +1100

    changed db_config

--- db_config.rb
+++ db_config.rb
@@ -6,4 +6,5 @@ options = {
 	database: 'stretchapp'
 }
 
-ActiveRecord::Base.establish_connection(options)
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || options)
+# ActiveRecord::Base.establish_connection(options)
